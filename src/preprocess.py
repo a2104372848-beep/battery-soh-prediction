f97a98fb8f46397171e6cda0b53102d8d7e5e0a0
a2104372848-beep <a2104372848@gmail.com>
2026-03-24 21:31:44 +0800

    Add data preprocessing class

---
+++ src/preprocess.py
@@ -0,0 +1,15 @@
+class DataPreprocessor:
+    def __init__(self, data):
+        self.data = data
+
+    def clean_data(self):
+        # Implement data cleaning logic here
+        pass
+
+    def normalize_data(self):
+        # Implement data normalization logic here
+        pass
+
+    def transform_data(self):
+        # Implement any data transformations here
+        pass
